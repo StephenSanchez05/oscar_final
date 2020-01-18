a93d51723d8ea6fc412d77f8890dc815b8628a6b
Stephen Sanchez <stephensanchez05@gmail.com>
2020-01-18 02:43:13 +0000

    first commit

---
+++ lib/oscar_project.rb
@@ -0,0 +1,6 @@
+require "oscar_project/version"
+
+module OscarProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/oscar_project/version.rb
@@ -0,0 +1,3 @@
+module OscarProject
+  VERSION = "0.1.0"
+end
